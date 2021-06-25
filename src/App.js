f10ed94c5cd601c0527c70833d030b31910c7878
Mahjoub-Nasreddine <82827922+Mahjoub-Nasreddine@users.noreply.github.com>
2021-06-25 03:13:04 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from './Banner'
+import FilmItem from './FilmItem';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <FilmItem />
+    </div>
+  );
+}
+
+export default App;
